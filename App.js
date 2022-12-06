00ab32f44b127b9143be907a415e943f95116e9d
hintheflesh <htheman@protonmail.com>
2022-12-06 16:54:44 -0500

    hello

--- App.js
+++ App.js
@@ -26,7 +26,7 @@ export default function App() {
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
+    flex: 3,
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
