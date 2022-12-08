c467996a7d2b5c51f3091f481307554afe049b69
IsaiahFrimpong <isaiahfrimpong1@gmail.com>
2022-12-08 17:06:05 -0500

    layout

--- Pages/history.js
+++ Pages/history.js
@@ -1,17 +1,21 @@
-import { View, Text, Button, FlatList } from 'react-native'
+import { View, Text, Button, FlatList, StyleSheet, Image } from 'react-native'
 import React from 'react'
 import { connect } from 'react-redux'
 import { addToWatchLater, addToFavourites, addToHistory, addToSearchResult, clearSearchResult } from '../redux/actions/movies'
 
 const renderHistory = ({ item }) => (
-  <Text>{item.Title}</Text>
+  <View style = {styles.items}>
+  <Image  style= {styles.movie} source={{uri:item.Poster, width:200,height:200 }}/>
+  <Text style = {styles.itemsText}>{item.Title}</Text>
+  </View>
 );
 
 const History = ({ navigation, moviesState }) => {
   return (
-    <View>
+    <View style = {styles.container}>
       <Text>history</Text>
-      <FlatList data={moviesState.history} renderItem={renderHistory} />
+      <FlatList style ={styles.historyList}
+      data={moviesState.history} renderItem={renderHistory} />
       <Button title="Back" onPress={() => {
         clearSearchResult()
         navigation.navigate("Home")
@@ -20,7 +24,17 @@ const History = ({ navigation, moviesState }) => {
   )
 }
 
+const styles = StyleSheet.create({
+  historyList: {alignContent:"stretch", width: "100%"},
+  container:{flex: 1,justifyContent: 'center',paddingTop: 40,backgroundColor: "black",padding: 8,},
+  items:{borderWidth:1,padding:10,margin:5,borderRadius:5,backgroundColor:"crimson"},  
+  itemsText:{color:"white", fontSize:"40px" , fontFamily:"Chicon"} ,
+  movie:{alignSelf:"flex-end"}
+});
+
+
 const mapStateToProps = (moviesState) => (moviesState)
 const mapDispatchToProps = { addToWatchLater, addToFavourites, addToHistory, addToSearchResult, clearSearchResult }
 
 export default connect(mapStateToProps, mapDispatchToProps)(History)
+
